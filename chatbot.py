559728a824b2d684b23a92962982704a37a7d279
Rohan Kumar <36202705+rohankumar10@users.noreply.github.com>
2020-01-19 13:07:43 +0530

    Create chatbot.py

---
+++ chatbot.py
@@ -0,0 +1,96 @@
+from nltk.chat.util import Chat, reflections
+
+pairs = [
+    [
+        r"my name is (.*)",
+        ["Hello %1, How are you today ?", ]
+    ],
+    [
+        r"what is your name ?",
+        ["My name is ROBO and I'm a chatbot, And your friend! ", ]
+    ],
+    [
+        r"how are you ?",
+        ["I'm doing good\nHow about You ?", ]
+    ],
+    [
+        r"sorry (.*)",
+        ["Its alright", "Its OK, never mind", ]
+    ],
+    [
+        r"i'm (.*) doing good",
+        ["Nice to hear that", "Alright :)", ]
+    ],
+    [
+        r"hi|hey|hello",
+        ["Hello", "Hey there", ]
+    ],
+    [
+        r"(.*) age?",
+        ["I'm a computer program dude\nSeriously you are asking me this?", ]
+
+    ],
+    [
+        r"what (.*) want ?",
+        ["Make me an offer I can't refuse", ]
+
+    ],
+    [
+        r"(.*) created ?",
+        ["RK created me", ]
+    ],
+    [
+        r"(.*) (location|city) ?",
+        ['Delhi', ]
+    ],
+    [
+        r"how is weather in (.*)?",
+        ["Weather in %1 is awesome like always", "Man its too hot here in %1", "Too cold here in %1",
+         "Never even heard about %1"]
+    ],
+    [
+        r"i work in (.*)?",
+        ["%1 is an Amazing company, I have heard about it.", ]
+    ],
+    [
+        r"(.*)raining in (.*)",
+        ["No rain since last week here in %2", "Damn its raining too much here in %2"]
+    ],
+    [
+        r"how (.*) health(.*)",
+        ["I'm a computer program, so I'm always healthy ", ]
+    ],
+    [
+        r"(.*) (sports|game) ?",
+        ["I'm a very big fan of Football", ]
+    ],
+    [
+        r"who (.*) sportsperson ?",
+        ["Mesut Ozil", "MS Dhoni",]
+    ],
+    [
+        r"who (.*) (moviestar|actor)?",
+        ["Emilia Clarke"]
+    ],
+    [
+        r"Bye",
+        ["BBye take care. See you soon :) ", "It was nice talking to you. See you soon :)"]
+    ],
+]
+
+
+flag = True
+print("ROBO: My name is Robo. I will answer your queries about Chatbots. If you want to exit, type Bye!")
+while flag:
+    user_response = input()
+    user_response = user_response.lower()
+    if user_response != 'bye':
+        if user_response == 'thanks' or user_response == 'thank you':
+            flag = False
+            print("ROBO: You are welcome..")
+        else:
+            chat = Chat(pairs, reflections)
+            chat.converse()
+    else:
+        flag = False
+        print("ROBO: Bye! take care..")
